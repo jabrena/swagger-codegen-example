3cfd6ae3c706d61858881689e3e430e7a4950e39
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-02-02 23:03:59 +0100

    Adding new examples with an approach API First

---
+++ openapi-spring-petclinic-server/src/main/java/com/jab/ms/OwnerController.java
@@ -0,0 +1,22 @@
+package com.jab.ms;
+
+import com.jab.ms.openapi.gen.api.OwnerApi;
+import com.jab.ms.openapi.gen.model.OwnerDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController()
+@RequestMapping(value = "/api")
+public class OwnerController implements OwnerApi {
+
+    @Override
+    public ResponseEntity<List<OwnerDto>> listOwners(String lastName, String ifNoneMatch) {
+        var owner = new OwnerDto();
+        owner.setFirstName("Demo");
+        return ResponseEntity.ok().body(List.of(owner));
+    }
+
+}
